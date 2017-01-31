d3250f9abeb3c523aa4a59173d56f55a6c7a54a4
nickrdandrea <nickrdandrea@gmail.com>
2017-01-30 22:10:59 -0500

    Add cat images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+  
   def welcome
     @header = "This is the welcome page"
   end
@@ -12,7 +14,16 @@ class PagesController < ApplicationController
   end
 
   def kitten
-    requested_size = params[:size]
-    @kitten_url = "http://www.lorempixel.com/#{requested_size}/#{requested_size}/cats/"
+
   end
+
+  def kittens
+
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
 end
