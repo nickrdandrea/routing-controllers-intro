4143b18ac7d4f98b516cf63a68d7ef67a75a4f42
nickrdandrea <nickrdandrea@gmail.com>
2017-01-30 22:37:06 -0500

    Add route helpers and remove hard coded urls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
 
   def contest
     flash[:notice] = "Sorry, the contest has ended."
-    redirect_to "/welcome"
+    redirect_to welcome_url
   end
 
   def kitten
@@ -25,7 +25,7 @@ class PagesController < ApplicationController
   def secrets
     if params[:magic_word] != "palace"
       flash[:alert] = "Sorry, you're not authorized to see that page!"
-      redirect_to "/welcome"
+      redirect_to welcome_url
     end
   end
 
